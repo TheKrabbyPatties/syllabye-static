b50d87378da067d8685b06d5d362cbec22800328
olivia1117 <oadamic@gmail.com>
2024-09-17 09:05:16 -0500

    added more to the firebase database set up

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*imports for firebase*/
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set} from "firebase/database";
+
+
 const url = "https://syllabye-server.azurewebsites.net"
 
 async function ping(){
@@ -92,6 +97,37 @@ function changeDeptBanner(elem) {
   var image = document.getElementById("banner-img");
   image.src = elem.value;
 }
+
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCQthMiRyG7rs8x-bX-uaOgpOCwGxwDogk",
+
+  authDomain: "syllabye-7f9b8.firebaseapp.com",
+
+  databaseURL: "https://syllabye-7f9b8-default-rtdb.firebaseio.com",
+
+  projectId: "syllabye-7f9b8",
+
+  storageBucket: "syllabye-7f9b8.appspot.com",
+
+  messagingSenderId: "914730272947",
+
+  appId: "1:914730272947:web:cb70b9b64ab37d5d0fc8c6"
+
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+
+
 
 
 async function testFirebaseConnection() {
