9fe104407df813f47abfb921449e9c1527907466
olivia1117 <oadamic@gmail.com>
2024-10-28 21:03:41 -0500

    fixing a few typos in create.html and uncommenting a few lines in index.js

--- index.js
+++ index.js
@@ -29,20 +29,37 @@ const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const db = getDatabase(app);
 
+connectDatabaseEmulator(db, 'localhost', 9000); // Adjust port as needed
 
 
 // //testing the option to add in course materials to database
-document.getElementById("course-submit").addEventListener('click', function(e)
-{
-  set(ref(db, 'materials/' + document.getElementById("course-materials").value),{
+// document.getElementById("course-submit").addEventListener('click', function(e)
+// {
+//   set(ref(db, 'materials/' + document.getElementById("course-materials").value),{
 
   
+//     textbooks: document.getElementById("course-materials-textbooks").value,
+//     supplements: document.getElementById("course-materials-supplements").value
+
+//   });
+//   alert("Data entry successful!");
+// })
+
+document.getElementById("course-submit").addEventListener('click', function(e) {
+  console.log("Submit button clicked"); // Log statement
+  set(ref(db, 'materials/' + document.getElementById("course-materials").value), {
     textbooks: document.getElementById("course-materials-textbooks").value,
     supplements: document.getElementById("course-materials-supplements").value
-
+  })
+  .then(() => {
+    console.log("Data written to Firebase successfully!");
+    alert("Data entry successful!");
+  })
+  .catch((error) => {
+    console.error("Firebase write failed:", error);
+    alert("Failed to write data to Firebase.");
   });
-  alert("Data entry successful!");
-})
+});
 
 //updated code to be integrated with server-side 
 // Add event listener for submitting course materials to the server
@@ -154,6 +171,100 @@ function generateGridCells() {
     return gridCells.join('');
 }
 
+
+// // Turns the syllabus into JSON and sends it to the server-side
+// document.addEventListener("DOMContentLoaded", () => {
+//   function handleSubmit(event) {
+//       event.preventDefault();
+
+//       // Object to hold all JSON outputs
+//       const jsonData = {};
+
+//       // Select each div section individually
+//       const instructorInfo = document.querySelector('.instructor-info');
+//       const courseInformation = document.querySelector('.course-information');
+//       const courseMaterials = document.querySelector('.course-materials');
+
+//       // Extract data from Instructor Info section
+//       const instructorData = {};
+//       instructorInfo.querySelectorAll('input, textarea').forEach(input => {
+//           instructorData[input.name] = input.value;
+//       });
+//       jsonData['instructor-info'] = instructorData;
+
+//       // Extract data from Course Information section except for the checkbox days
+//       const courseData = {};
+//       courseInformation.querySelectorAll('input, textarea').forEach(input => {
+//           if (!(input.name == 'day')){
+//             courseData[input.name] = input.value;
+//           };
+//           // Properlly display the selected term length
+//           if (input.name == 'term'){
+//             document.querySelectorAll('input[type="radio"]:checked').forEach(radio => {
+//               courseData[input.name] = radio.id;
+//             });
+//             if (courseData[input.name] == "on") {
+//               courseData[input.name] = "";
+//             }
+//           };
+//       });
+      
+//       // Extract the meeting days in one list
+//       const days = [];
+//       courseInformation.querySelectorAll('input[type="checkbox"]:checked').forEach(checkbox => {
+//           days.push(checkbox.value);
+//       });
+//       courseData['meeting-days'] = days;
+//       jsonData['course-information'] = courseData;
+
+//       // Extract data from Course Materials section
+//       const materialsData = {};
+//       courseMaterials.querySelectorAll('input').forEach(input => {
+//           materialsData[input.name] = input.value;
+//       });
+//       jsonData['course-materials'] = materialsData;
+
+//             // Log JSON outputs
+//       console.log(JSON.stringify(jsonData, null, 4));
+
+//       // Convert JSON to string
+//       var jsonString = JSON.stringify(jsonData);
+//       // Convert JSON to string and send it to Firebase
+//       console.log("Writing to Firebase at:", syllabusRef);
+//       console.log("Data being sent:", jsonData);
+//       const syllabusRef = ref(db, 'syllabus/' + jsonData['course-information']['courseName']);
+//       set(syllabusRef, jsonData)
+//         .then(() => {
+//           console.log("Data written to Firebase successfully!");
+//           alert("Data entry successful!");
+//         })
+//         .catch((error) => {
+//           console.error("Firebase write failed:", error);
+//           alert("Failed to write data to Firebase.");
+//         });
+//   }
+
+
+  //     // Send the data to the server
+  //     fetch('https://syllabye-server.azurewebsites.net/save/json', {
+  //       method: 'POST',
+  //       headers: {
+  //           'Content-Type': 'application/json'
+  //       },
+  //       body: jsonString
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => console.log('Success:', data))
+  //     .catch((error) => console.error('Error:', error));
+  // }
+
+  // const form = document.querySelector('#form');
+  // form.addEventListener('submit', handleSubmit);
+// });
+
+
+
+
 // async function testFirebaseConnection() {
 //   try {
 //     // Reference to a location in your database
@@ -180,79 +291,3 @@ function generateGridCells() {
 
 
 
-
-
-// Turns the syllabus into JSON and sends it to the server-side
-document.addEventListener("DOMContentLoaded", () => {
-  function handleSubmit(event) {
-      event.preventDefault();
-
-      // Object to hold all JSON outputs
-      const jsonData = {};
-
-      // Select each div section individually
-      const instructorInfo = document.querySelector('.instructor-info');
-      const courseInformation = document.querySelector('.course-information');
-      const courseMaterials = document.querySelector('.course-materials');
-
-      // Extract data from Instructor Info section
-      const instructorData = {};
-      instructorInfo.querySelectorAll('input, textarea').forEach(input => {
-          instructorData[input.name] = input.value;
-      });
-      jsonData['instructor-info'] = instructorData;
-
-      // Extract data from Course Information section except for the checkbox days
-      const courseData = {};
-      courseInformation.querySelectorAll('input, textarea').forEach(input => {
-          if (!(input.name == 'day')){
-            courseData[input.name] = input.value;
-          };
-          // Properlly display the selected term length
-          if (input.name == 'term'){
-            document.querySelectorAll('input[type="radio"]:checked').forEach(radio => {
-              courseData[input.name] = radio.id;
-            });
-            if (courseData[input.name] == "on") {
-              courseData[input.name] = "";
-            }
-          };
-      });
-      
-      // Extract the meeting days in one list
-      const days = [];
-      courseInformation.querySelectorAll('input[type="checkbox"]:checked').forEach(checkbox => {
-          days.push(checkbox.value);
-      });
-      courseData['meeting-days'] = days;
-      jsonData['course-information'] = courseData;
-
-      // Extract data from Course Materials section
-      const materialsData = {};
-      courseMaterials.querySelectorAll('input').forEach(input => {
-          materialsData[input.name] = input.value;
-      });
-      jsonData['course-materials'] = materialsData;
-
-      // Log JSON outputs
-      console.log(JSON.stringify(jsonData, null, 4));
-
-      // Convert JSON to string
-      var jsonString = JSON.stringify(jsonData);
-
-      // Send the data to the server
-      fetch('https://syllabye-server.azurewebsites.net/save/json', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: jsonString
-      })
-      .then(response => response.json())
-      .then(data => console.log('Success:', data))
-      .catch((error) => console.error('Error:', error));
-  }
-
-  const form = document.querySelector('#form');
-  form.addEventListener('submit', handleSubmit);
-});
