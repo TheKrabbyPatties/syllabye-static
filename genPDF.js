adf4248503254c767f35901f9653735535b7138a
BryanA55 <bryanhavalos@lewisu.edu>
2024-11-17 19:18:53 -0600

    Updating genPDF.js

--- genPDF.js
+++ genPDF.js
@@ -1,34 +1,35 @@
-document.getElementById('btn-to-pdf').onclick = function() {
-    // Check if formObject and its properties are populated
-    const courseName = formObject['course-information'] ? formObject['course-information'].courseName : 'Unknown Course';
-    const courseNumber = formObject['course-information'] ? formObject['course-information'].courseNumber : '000';
-    const courseSection = formObject['course-information'] ? formObject['course-information'].courseSection : 'A';
+window.onload = function() {
+    document.getElementById('btn-to-pdf').onclick = function() {
+        const courseName = formObject['course-information'] ? formObject['course-information'].courseName : 'Unknown Course';
+        const courseNumber = formObject['course-information'] ? formObject['course-information'].courseNumber : '000';
+        const courseSection = formObject['course-information'] ? formObject['course-information'].courseSection : 'A';
 
-    const docName = courseName + " (" +
-        courseNumber + "-" +
-        courseSection + ")";
+        const docName = courseName + " (" +
+            courseNumber + "-" +
+            courseSection + ")";
 
-    
-    Export2PDF('content', docName); 
+        Export2PDF('content', docName); 
+    };
 };
+
 
 function Export2PDF(element, filename = 'document') {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
 
-    // Get the HTML content from the specified element
+    // Get the HTML content
     var html = document.getElementById(element);
 
     // Capture the content of the element as HTML text
     doc.html(html, {
         callback: function(doc) {
-            // Save the PDF with the given filename
+            // Save the PDF with the filename
             doc.save(filename + '.pdf');
         },
-        x: 10, // Set the X coordinate for the start of content in the PDF
-        y: 10, // Set the Y coordinate for the start of content in the PDF
+        x: 10, // Set the X coordinate 
+        y: 10, // Set the Y coordinate 
         html2canvas: {
-            scale: 0.5 // Adjust scale to fit the content better
+            scale: 0.5 // Adjust scale to fit the content
         }
     });
 }
