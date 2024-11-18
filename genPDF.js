b36d5087a55612dfb22c5a37e1117f61303ca30b
BryanA55 <bryanhavalos@lewisu.edu>
2024-11-17 19:02:58 -0600

    Fixing download as PDF

--- genPDF.js
+++ genPDF.js
@@ -1,19 +1,26 @@
 document.getElementById('btn-to-pdf').onclick = function() {
-    const docName = formObject['course-information'].courseName + " (" +
-        formObject['course-information'].courseNumber + "-" +
-        formObject['course-information'].courseSection + ")";
-    Export2PDF('content', docName);
+    // Check if formObject and its properties are populated
+    const courseName = formObject['course-information'] ? formObject['course-information'].courseName : 'Unknown Course';
+    const courseNumber = formObject['course-information'] ? formObject['course-information'].courseNumber : '000';
+    const courseSection = formObject['course-information'] ? formObject['course-information'].courseSection : 'A';
+
+    const docName = courseName + " (" +
+        courseNumber + "-" +
+        courseSection + ")";
+
+    
+    Export2PDF('content', docName); 
 };
 
-function Export2PDF(elementId, filename = 'document') {
+function Export2PDF(element, filename = 'document') {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
 
     // Get the HTML content from the specified element
-    const element = document.getElementById(elementId);
+    var html = document.getElementById(element);
 
     // Capture the content of the element as HTML text
-    doc.html(element, {
+    doc.html(html, {
         callback: function(doc) {
             // Save the PDF with the given filename
             doc.save(filename + '.pdf');
